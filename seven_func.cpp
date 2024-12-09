0599fcc436d74295f9f46b243143cf72a42125ce
likhtarev <geralemaa@gmail.com>
2024-12-09 21:10:06 +0300

    Create seven_func.cpp

---
+++ seven_func.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cstring>
+#include <cstdlib>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <netinet/ip_icmp.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <errno.h>
+
+// Функция для расчета контрольной суммы
+unsigned short checksum(void *b, int len) {
+    unsigned short *buf = (unsigned short *)b;
+    unsigned int sum = 0;
+    unsigned short result;
+
+    for (sum = 0; len > 1; len -= 2) {
+        sum += *buf++;
+    }
+    if (len == 1) {
+        sum += *(unsigned char *)buf;
+    }
+    sum = (sum >> 16) + (sum & 0xFFFF);
+    sum += (sum >> 16);
+    result = ~sum;
+    return result;
+}
+
+// Функция для измерения времени
+double time_diff(struct timeval *start, struct timeval *end) {
+    return (double)(end->tv_sec - start->tv_sec) * 1000.0 + (end->tv_usec - start->tv_usec) / 1000.0;
+}
